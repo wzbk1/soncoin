b17b03dbb1c9f157116d16219b404dab54f4db87
wzbk1 <161694279+wzbk1@users.noreply.github.com>
2024-06-02 11:23:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function transferTokens() {
     const amount = document.getElementById('amount').value;
 
     if (recipientAddress && amount > 0) {
-        document.getElementById('message').innerText = `Successfully transferred ${amount} TON to ${recipientAddress}!`;
-        document.getElementById('recipient-address').value = '';
-        document.getElementById('amount').value = '';
+        const tonkeeperUrl = `https://app.tonkeeper.com/transfer/${recipientAddress}?amount=${amount}`;
+        window.open(tonkeeperUrl, '_blank');
+        document.getElementById('message').innerText = `Transferring ${amount} TON to ${recipientAddress}`;
     } else {
         document.getElementById('message').innerText = 'Please enter a valid recipient address and amount.';
     }
